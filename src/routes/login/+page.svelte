108d1c77043a2834999e7b766133cf6bc46ed8ad
Tobias Skarhed <tobias.skarhed@gmail.com>
2025-09-28 20:18:09 +0200

    Use form styles for login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -28,10 +28,45 @@
 	}
 </script>
 
-<h2>Logga in</h2>
-<form onsubmit={login} id="login-form">
-	<input bind:value={email} placeholder="Email" />
-	<input type="password" bind:value={password} placeholder="Lösenord" />
-	<button type="submit">Logga in</button>
-</form>
-<p>{error}</p>
+<section id="login-form">
+	<h2>Logga in</h2>
+	<form onsubmit={login}>
+		<input bind:value={email} placeholder="Email" required />
+		<input type="password" bind:value={password} placeholder="Lösenord" required />
+		<button type="submit">Logga in</button>
+		<button type="button" onclick={signup}>Registrera</button>
+	</form>
+	{#if error}
+		<p class="error-details">{error}</p>
+	{/if}
+</section>
+
+<style>
+	#login-form {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	#login-form form {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 0.5rem;
+	}
+
+	form input {
+		grid-column: span 2;
+	}
+
+	button {
+		width: 100%;
+	}
+
+	.error-details {
+		color: #d32f2f !important;
+		font-style: italic;
+		font-size: 0.9rem;
+		text-align: center;
+		margin: 0.5rem 0;
+	}
+</style>
